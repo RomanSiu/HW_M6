c731e3d5cb4378a5ab45af6884f77519f214e923
RomanSiu <siusiailoroman@gmail.com>
2023-08-26 14:14:08 +0300

    Final2

--- Sort_grbg.py
+++ Sort_grbg.py
@@ -6,7 +6,33 @@ import shutil
 def normalize(text, suf = None):
     import re
     
-    letters = {
+    str1 = ""
+    
+    text = text[:-(len(suf))] if suf else ...
+    
+    text = re.sub("\W+", "_", text)
+    
+    for let in text:
+        if let.islower():
+            str1 += let.translate(letters)
+        elif let.isupper():
+            let = let.lower()
+            up_let = let.translate(letters)
+            str1 += up_let.upper()
+        else:
+            str1 += let
+    
+    str1 += suf if suf else ...
+    
+    return str1
+        
+try:
+    param = argv[1]
+except IndexError:
+    print("Use path as a param")
+    exit()
+    
+letters = {
         ord('а'): 'a',
         ord('б'): 'b',
         ord('в'): 'v',
@@ -41,27 +67,19 @@ def normalize(text, suf = None):
         ord('ю'): 'yu',
         ord('я'): 'ya'
     }
-    
-    str1 = ""
-    
-    text = text[:-(len(suf))] if suf else ...
-    
-    text = re.sub("\W+", "_", text)
-    
-    for let in text:
-        if let.islower():
-            str1 += let.translate(letters)
-        elif let.isupper():
-            let = let.lower()
-            up_let = let.translate(letters)
-            str1 += up_let.upper()
-        else:
-            str1 += let
-    
-    str1 += suf if suf else ...
-    
-    return str1
+folder_list = ["archives", "videos", "images", "documents", "audio", "other"]
+suf_dict = {"img": [".jpeg", ".jpg", ".png", ".svg"],
+            "vid": [".avi", ".mp4", ".mov", ".mkv"],
+            "doc": [".doc", ".docx", ".txt", ".pdf", ".xlsx", ".pptx"],
+            "mus": [".mp3", ".ogg", ".wav", ".amr"],
+            "arch": [".zip", ".gz", ".tar"]}
+files_dict = {"img": [], "vid": [], "doc": [], "mus": [], "arch": [], "other": []}
+inc_suf = []
+unknown_suf = []
 
+param1 = " ".join(argv[1:])
+path_grbg = Path(fr"{param1}")    
+           
 def sorting(path):
     
     for file in path.iterdir():
@@ -78,23 +96,23 @@ def sorting(path):
         if suf in suf_dict['img']:
             files_dict['img'].append(new_file_name)
             inc_suf.append(suf) if not suf in inc_suf else ...
-            new_file_path = r"C:\Users\User\OneDrive\Рабочий стол\Garbage\images\\" + new_file_name
+            new_file_path = str(path_grbg) + r"\images\\" + new_file_name
         elif suf in suf_dict['doc']:
             files_dict['doc'].append(new_file_name)
             inc_suf.append(suf) if not suf in inc_suf else ...
-            new_file_path = r"C:\Users\User\OneDrive\Рабочий стол\Garbage\documents\\" + new_file_name
+            new_file_path = str(path_grbg) + r"\documents\\" + new_file_name
         elif suf in suf_dict['mus']:
             files_dict['mus'].append(new_file_name)
             inc_suf.append(suf) if not suf in inc_suf else ...
-            new_file_path = r"C:\Users\User\OneDrive\Рабочий стол\Garbage\audio\\" + new_file_name
+            new_file_path = str(path_grbg) + r"\audio\\" + new_file_name
         elif suf in suf_dict['vid']:
             files_dict['vid'].append(new_file_name)
             inc_suf.append(suf) if not suf in inc_suf else ...
-            new_file_path = r"C:\Users\User\OneDrive\Рабочий стол\Garbage\video\\" + new_file_name
+            new_file_path = str(path_grbg) + r"\video\\" + new_file_name
         elif suf in suf_dict['arch']:
             files_dict['arch'].append(new_file_name)
             inc_suf.append(suf) if not suf in inc_suf else ...
-            new_file_path = r"C:\Users\User\OneDrive\Рабочий стол\Garbage\archives\\" + new_file_name[:-(len(suf))]
+            new_file_path = str(path_grbg) + r"\archives\\" + new_file_name[:-(len(suf))]
             os.mkdir(new_file_path)
             shutil.unpack_archive(file, new_file_path)
             os.remove(file)
@@ -102,31 +120,12 @@ def sorting(path):
         else:
             files_dict['other'].append(new_file_name)
             unknown_suf.append(suf) if not suf in inc_suf else ...
-            new_file_path = r"C:\Users\User\OneDrive\Рабочий стол\Garbage\other\\" + new_file_name
+            new_file_path = str(path_grbg) + r"\other\\" + new_file_name
         
         os.rename(file, new_file_path)
-        
-    print(files_dict)
-    print(inc_suf)
-    print(unknown_suf)
-        
-try:
-    param = argv[1:]
-except IndexError:
-    print("Use path as a param")
-    exit()
-
-folder_list = ["archives", "videos", "images", "documents", "audio", "other"]
-suf_dict = {"img": [".jpeg", ".jpg", ".png", ".svg"],
-            "vid": [".avi", ".mp4", ".mov", ".mkv"],
-            "doc": [".doc", ".docx", ".txt", ".pdf", ".xlsx", ".pptx"],
-            "mus": [".mp3", ".ogg", ".wav", ".amr"],
-            "arch": [".zip", ".gz", ".tar"]}
-files_dict = {"img": [], "vid": [], "doc": [], "mus": [], "arch": [], "other": []}
-inc_suf = []
-unknown_suf = []
-
-param1 = " ".join(param)
-path_grbg = Path(fr"{param1}")               
-       
+         
 sorting(path_grbg)
+
+print(files_dict)
+print(inc_suf)
+print(unknown_suf)
